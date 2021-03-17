1a05f35a0d64c0feefa67fb1d34a063fea1b68c7
mironenko <mironenko@protei.ru>
2021-03-17 13:49:25 +0300

    dinamic calculator

--- chapter1/js/func_app_ui.js
+++ chapter1/js/func_app_ui.js
@@ -23,3 +23,75 @@ function multiplyNumeric(obj) {
         }
     }
 }
+
+function Calculator(a, b) {
+    this.a = Number(a);
+    this.b = Number(b);
+
+    this.sum = function () {
+        return this.a + this.b;
+    };
+
+    this.mul = function () {
+        return this.a * this.b;
+    };
+}
+
+function filterRange(arr, a, b) {
+    return arr.filter(val => (a <= val && val <= b));
+}
+
+function compareNumeric(a, b) {
+    return a < b;
+}
+
+function copyAndSortArray(arr) {
+    return arr.slice().sort();
+}
+
+function DinamicCalculator() {
+    this.methods = [];
+    const digitsOnly = (val) => /^[0-9]+$/.test(val);
+
+    this.addMethod = function (method, func) {
+        this.methods.push({
+            [method]: func
+        });
+    }
+
+    this.calculate = function (line) {
+        let array = line.split(' ');
+        let a = Number(array[0]);
+        let b = Number(array[2]);
+
+        if (!validateArray(array)) {
+            return 'incorrected input string';
+        }
+
+        let method;
+
+        for (let i = 0; i < this.methods.length; i++) {
+            for (let key in this.methods[i]) {
+                if (key == array[1]) {
+                    method = this.methods[i];
+                    break;
+                }
+            }
+            if (validateMethod(method)) {
+                break;
+            }
+        }
+        if (!validateMethod(method)) {
+            return 'method is not supported';
+        }
+        return method[array[1]](a, b);
+    }
+
+    const validateMethod = (method) => {
+        return typeof method != 'undefined';        
+    }
+
+    const validateArray = (array) => {
+        return Array.isArray(array) && array.length == 3 && digitsOnly(array[0]) && digitsOnly(array[2]) && !digitsOnly(array[1]);
+    }
+}
--- chapter1/js/test.js
+++ chapter1/js/test.js
@@ -16,3 +16,85 @@ describe("multiplyNumeric", function () {
     });
 
 });
+
+describe("Caclculator", function () {
+    let calc = new Calculator('3','2')
+    it("сложить 3 с 2", function () {        
+        assert.equal(calc.sum(), 5);
+    });
+
+    it("умножить 3 на 2", function () {        
+        assert.equal(calc.mul(), 6);
+    });
+});
+
+describe("Filter array", function () {
+    it("filter to 1 from 4", function () {
+        expect([1,3,4]).to.have.members(filterRange([1,3,4,5,6,7], 1, 4));       
+    });
+
+    it("filter to 4 from 8", function () {
+        expect([4,5,6,7]).to.have.members(filterRange([10,3,4,5,6,7], 4, 8));       
+    });
+
+    it("save start state by array", function () {
+        var arr = [10,3,4,5,6,7];
+        var tmp = filterRange(arr, 4, 8);
+        expect([10,3,4,5,6,7]).to.have.members(arr);       
+    });
+});
+
+describe("Sorted array", function () {
+    it("sorted start to 10 end to 1", function () {
+        var arr = [10,1,9,1,8,7];
+        expect([10,9,8,7,1,1]).to.have.members(arr.sort(compareNumeric));
+        expect([10,9,8,7,1,1]).to.have.members(arr.sort((a,b) => a < b));       
+    });
+});
+
+describe("Copy and sorted array", function () {
+    it("copy and sort start to 10 end to 1", function () {
+        var arr = [10,1,9,1,8,7];
+        var tmp = copyAndSortArray(arr);
+        expect([10,9,8,7,1,1]).to.have.members(tmp);
+        expect([10,9,8,7,1,1]).to.have.members(arr);        
+    });
+});
+
+describe("Dinamic Caclculator", function () {
+    let calc = new DinamicCalculator();
+    calc.addMethod('*', (a, b) => a * b);   
+    calc.addMethod('+', (a, b) => a + b);
+    calc.addMethod('**', (a, b) => a ** b);
+    calc.addMethod('-', (a, b) => a - b);  
+    calc.addMethod('/', (a, b) => a / b);  
+
+    it("сложить 2 + 3", function () {        
+        assert.equal(5, calc.calculate("2 + 3"));
+    });
+
+    it("умножить 2 * 3", function () {        
+        assert.equal(6, calc.calculate("2 * 3"));
+    });
+
+    it("вычесть 2 - 3", function () {        
+        assert.equal(-1, calc.calculate("2 - 3"));
+    });
+
+    it("возвести 2 в степень 3", function () {        
+        assert.equal(8, calc.calculate("2 ** 3"));
+    });
+
+    it("деление 10 / 2", function () {        
+        assert.equal(5, calc.calculate("10 / 2"));
+    });
+
+    it("неверный формат ввода строки", function () {        
+        assert.equal('incorrected input string', calc.calculate("10 1 2"));
+    });
+
+    it("неверный формат ввода математического метода", function () {        
+        assert.equal('method is not supported', calc.calculate("10 -*/ 2"));
+    });  
+    
+});
